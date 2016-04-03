d53e78e5df16bd11c1181619a10188991c295d3f
Eray Diler <eray.diler@gmail.com>
2016-04-03 19:39:11 +0300

    Initialized

---
+++ GoogleCalendarApi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleCalendarApi
+//
+//  Created by Eray on 03/04/16.
+//  Copyright © 2016 HipoBlog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoogleCalendarApi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleCalendarApi
+//
+//  Created by Eray on 03/04/16.
+//  Copyright © 2016 HipoBlog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
