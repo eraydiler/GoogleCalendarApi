5bc72ccf76ef82dbb40bed5c1d6a3ee2ee53729d
Eray Diler <eray.diler@gmail.com>
2016-04-03 20:31:21 +0300

    Initial commit.

--- GoogleCalendarApi/AppDelegate.m
+++ GoogleCalendarApi/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    UIWindow *window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    ViewController *controller = [[ViewController alloc] init];
+
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:controller];
+
+    [window setRootViewController:navController];
+    [window makeKeyAndVisible];
+
+    [self setWindow:window];
+
     return YES;
 }
 
