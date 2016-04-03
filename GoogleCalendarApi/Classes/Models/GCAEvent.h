60a890ad1ded69fbafee197bf4b362c3dfa5da25
Eray Diler <eray.diler@gmail.com>
2016-04-03 22:13:52 +0300

    Reorganised folder/group hiearchy. Added event model class.

---
+++ GoogleCalendarApi/Classes/Models/GCAEvent.h
@@ -0,0 +1,13 @@
+//
+//  GCAEvent.h
+//  GoogleCalendarApi
+//
+//  Created by Eray on 03/04/16.
+//  Copyright © 2016 HipoBlog. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCAEvent : NSObject
+
+@end
---
+++ GoogleCalendarApi/Classes/Models/GCAEvent.m
@@ -0,0 +1,13 @@
+//
+//  GCAEvent.m
+//  GoogleCalendarApi
+//
+//  Created by Eray on 03/04/16.
+//  Copyright © 2016 HipoBlog. All rights reserved.
+//
+
+#import "GCAEvent.h"
+
+@implementation GCAEvent
+
+@end
