c3a120063624cc57cf4d823982e460dc5e5db6b5
Eray Diler <eray.diler@gmail.com>
2016-04-03 19:39:11 +0300

    Initialized

---
+++ GoogleCalendarApi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoogleCalendarApi
+//
+//  Created by Eray on 03/04/16.
+//  Copyright © 2016 HipoBlog. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GoogleCalendarApi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoogleCalendarApi
+//
+//  Created by Eray on 03/04/16.
+//  Copyright © 2016 HipoBlog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
